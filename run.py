8da7330afd471869b0eb1d8b0920aff64a67d98d
Darragh <darraghdrennan5gmail.com>
2024-02-12 19:04:38 +0000

    Create Hangman class

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-WELCOME TO HANGMAN! 
+print("WELCOME TO HANGMAN!") 
+
+import random 
+
+class Hangman: 
+    def __init__(self, word_bank, difficulty):
+        self.word_bank = word_bank
+        self.difficulty
+
