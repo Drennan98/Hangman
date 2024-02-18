a8ad021527d9d13177de9d80a08c1690235b7409
Darragh <darraghdrennan5gmail.com>
2024-02-18 19:11:54 +0000

    Fix error in Hangman preventing the game from running

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ class Hangman:
     def __init__(self, word_bank, difficulty):
         self.word_bank = word_bank
         self.difficulty = difficulty
-        self.secret_word = self.secret_word()
+        self.secret_word = self.select_word()
         self.guesses_left = 6 
         self.guessed_letters = set()
         self.display_word = ['_'] * len(self.secret_word)
